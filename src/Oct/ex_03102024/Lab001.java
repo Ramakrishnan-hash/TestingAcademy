0f001d5da1b968cc9a8ddc8c9d2acd88a66fbb4a
Ramkrishnan-hash <randyramki1505@gmail.com>
2024-10-04 09:54:08 +0530

    First Commit

---
+++ src/Oct/ex_03102024/Lab001.java
@@ -0,0 +1,10 @@
+package Oct.ex_03102024;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        int num = 10;
+        for (int i =1; i<=10;i++){
+            System.out.println(num+"x"+i+"="+(num*i));
+        }
+    }
+}
---
+++ src/Oct/ex_03102024/Lab002.java
@@ -0,0 +1,12 @@
+package Oct.ex_03102024;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        int x = 20, y = 10;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+    }
+}
