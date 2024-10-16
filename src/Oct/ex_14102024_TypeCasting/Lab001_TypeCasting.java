89153ed6ddceb8ea8bb593cf27d47e5c108f35f2
Ramkrishnan-hash <randyramki1505@gmail.com>
2024-10-16 09:39:25 +0530

    First Commit

---
+++ src/Oct/ex_14102024_TypeCasting/Lab001_TypeCasting.java
@@ -0,0 +1,17 @@
+package Oct.ex_14102024_TypeCasting;
+
+public class Lab001_TypeCasting {
+    public static void main(String[] args) {
+       /* Real-Life Example
+ Here's a real-life example of type casting where we create a program to calculate the percentage
+ of a user's score in relation to the maximum score in a game.
+
+ We use type casting to make sure that the result is a floating-point value, rather than an integer:
+        */
+int max_score = 500, user_score = 461;
+
+double total_percentage = (double)user_score/max_score*100.00d;
+
+System.out.println(total_percentage);
+    }
+}
